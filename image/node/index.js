951f9ddc46db06b3d92bf0ba4fb83bb3a1b80399
pedro-hca <pedroandradecontato@hotmail.com>
2024-02-19 16:57:17 -0300

    using nodejs with a mysql database and persisting the data

--- image/node/index.js
+++ image/node/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
 
-app.get('/', (req,res) => {
+const sql = `INSERT INTO people(name) values('Pedro')`
+connection.query(sql)
+connection.end()
+
+app.get('/', (req, res) => {
     res.send('<h1>Running node in a container</h1>')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Running on port ' + port)
 })
